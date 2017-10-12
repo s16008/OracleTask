68efb96657faa6d2c8e17bb488f80f881ca5d289
Yuya Minami <s16008@std.it-college.ac.jp>
2017-10-12 13:59:20 +0900

    昇順になるよう修正

--- 1011/1011.5.sql
+++ 1011/1011.5.sql
@@ -1,3 +1,5 @@
 select last_name,hire_date,to_char(next_day(add_months(hire_date,6),'mon'),'fmDay, "the" Ddthsp " of " Month, YYYY',
 'nls_date_language = AMERICAN') as "REVIEW"
-from employees;
+from employees
+order by employee_id
+fetch first 5 rows only;
