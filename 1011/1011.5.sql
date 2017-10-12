53f18f3b786ea8ae19e4ac022e10fd43a3398d1a
Yuya Minami <s16008@std.it-college.ac.jp>
2017-10-12 14:09:10 +0900

    5番目の課題を完成（多分）

--- 1011/1011.5.sql
+++ 1011/1011.5.sql
@@ -1,4 +1,5 @@
-select last_name,hire_date,to_char(next_day(add_months(hire_date,6),'mon'),'fmDay, "the" Ddthsp " of " Month, YYYY',
+select last_name,to_char(hire_date,'DD-MON-YY'),
+				to_char(next_day(add_months(hire_date,6),'mon'),'fmDay, "the" Ddthsp " of " Month, YYYY',
 'nls_date_language = AMERICAN') as "REVIEW"
 from employees
 order by employee_id
