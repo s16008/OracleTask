0c63061f60f510b7ab890c740da84f691f681657
Yuya Minami <s16008@std.it-college.ac.jp>
2017-10-17 16:57:02 +0900

    4と5を修正

--- 1017/1017.4.sql
+++ 1017/1017.4.sql
@@ -1,4 +1,4 @@
-select e.last_name,e.employee_id,m.last_name,e.manager_id
+select e.last_name "Employee",e.employee_id "Emp#",m.last_name "Manager",e.manager_id "Mgr#"
 from employees e join employees m
 --on m.employee_id = e.manager_id
 on e.manager_id = m.employee_id;
