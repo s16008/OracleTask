2e62eea9b4a98428c7bec939f5641157124932e1
Yuya Minami <s16008@std.it-college.ac.jp>
2017-11-16 16:51:42 +0900

    11月16日の課題を追加

---
+++ 1116/1116.1.sql
@@ -0,0 +1,3 @@
+update title
+set price = &price
+where title = '&title';
---
+++ 1116/1116.2.sql
@@ -0,0 +1,3 @@
+select m.last_name,t.title,r.book_date, r.act_ret_date - r.book_date as duration
+from member m join rental r on (m.member_id = r.member_id)
+join title t on (t.title_id = r.title_id);
