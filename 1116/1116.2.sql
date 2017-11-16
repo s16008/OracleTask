e957c0bcd3f63a821211a248d321da4c2259f585
Yuya Minami <s16008@std.it-college.ac.jp>
2017-11-16 16:53:16 +0900

    顧客名でソートするよう修正

--- 1116/1116.2.sql
+++ 1116/1116.2.sql
@@ -1,3 +1,4 @@
 select m.last_name,t.title,r.book_date, r.act_ret_date - r.book_date as duration
 from member m join rental r on (m.member_id = r.member_id)
-join title t on (t.title_id = r.title_id);
+join title t on (t.title_id = r.title_id)
+order by m.last_name;
