a7e75cc78d7da9e060f806e426c1c4b7cd9eb637
Yuya Minami <s16008@std.it-college.ac.jp>
2017-11-29 13:38:38 +0900

    4の内容を修正

--- 1128/1128.4.sql
+++ 1128/1128.4.sql
@@ -1,3 +1,3 @@
-select employee_id,last_name,to_char(hire_date, 'fmDD DAY YYYY','nls_date_language =AMERICAN')
+select employee_id,last_name,to_char(hire_date, 'fmDD Month YYYY','nls_date_language =AMERICAN')
 from employees
 order by employee_id;
